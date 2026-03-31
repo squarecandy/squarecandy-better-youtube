686c0c388fde8cc006272e4f73e77ef6a98aaa00
Eileen Mack <eileenmack@gmail.com>
2026-03-31 17:02:07 -0400

    fix: match current arrow styling

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 define( 'SQUARECANDY_BYT_PATH', plugin_dir_path( __FILE__ ) );
 define( 'SQUARECANDY_BYT_URL', plugin_dir_url( __FILE__ ) );
-define( 'SQUARECANDY_BYT_VERSION', 'version-1.3.2-develop.1' );
+define( 'SQUARECANDY_BYT_VERSION', 'version-1.3.2-develop.0' );
 
 // Square Candy Common files
 require_once SQUARECANDY_BYT_PATH . '/inc/sqcdy-common.php';
@@ -191,7 +191,7 @@ function better_youtube_api_playlist( $input ) {
 			<div class="playlist-thumb" style="background-image:url(' .
 				$large_thumb . ')"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg></div>
 			</div></div>';
-		$output .= '<div class="nav-buttons"><span class="nav-button previous mfp-arrow mfp-arrow-left mfp-prevent-close" data-video-index="">Previous</span><span class="nav-button next mfp-arrow mfp-arrow-right mfp-prevent-close" data-video-index="1">Next</span></div>';
+		$output .= '<div class="nav-buttons"><button class="nav-button previous mfp-arrow mfp-arrow-left mfp-prevent-close" data-video-index=""></button><button class="nav-button next mfp-arrow mfp-arrow-right mfp-prevent-close" data-video-index="1"></button></div>';
 
 		//set up the html for all items (small display)
 		$output .= '<div class="playlist-list"><ul>';
